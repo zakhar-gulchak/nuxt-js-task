ddc55653339bc37df8e858443a2cb7c7354d5145
Zakhar Gulchak <gulchak.zahar@gmail.com>
2023-05-05 14:35:09 +0200

    chore: adjusted components dir structure to atomic model

---
+++ client/components/atoms/Tag.vue
@@ -0,0 +1,21 @@
+<template>
+    <span class="inline-block rounded-md p-2" :style="`background-color: #${color || 'ddd'};`">
+    {{ name }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: false,
+    },
+    color: {
+      type: String,
+    }
+  },
+}
+</script>
+
+<style></style>
